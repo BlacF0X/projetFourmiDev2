41c76dce2af35d9e2147cb220f2081bea39d6818
Breit <l.breitenstein@students.ephec.be>
2024-12-03 12:29:12 +0100

    Ajout de larve.py

--- scripts/classes/colonies.py
+++ scripts/classes/colonies.py
@@ -5,6 +5,7 @@ import random
 
 from scripts.classes.reine import Reine
 from scripts.classes.ouvriere import Ouvriere
+from scripts.classes.larve import Larve
 
 
 
@@ -15,17 +16,6 @@ from scripts.classes.ouvriere import Ouvriere
 
 
 
-class Larve:
-    def __init__(self, speed, life, ratio, force, tm_to_spawn=100, reine=False, ):
-        self.time_to_spawn = tm_to_spawn
-        if reine:
-            self.role = 'reine'
-        else:
-            self.role = 'ouvriere'
-        self.speed = speed
-        self.life = life
-        self.ratio = ratio
-        self.force = force
 
 
 class Colonie:
